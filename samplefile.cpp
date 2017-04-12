da4ca9aba1d8979e59c899fde950f2c379d2a5d7
apmechev <apmechev@gmail.com>
2017-04-12 18:50:06 +0200

    Can now launch programs with arguments using -l <command surrounded by quotes> or similar

--- samplefile.cpp
+++ samplefile.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>    /*remove_if*/
 #include <libgen.h>     /*basename()*/
 #include <getopt.h>	/*option*/
+#include <vector>	/*vector for execvp*/
 //=============
 //=============
 
@@ -106,6 +107,39 @@ getstat(const std::string& pid,std::fstream& tsdbfile,std::string metric)//Get m
     return;
 }
 
+int
+launch_process(std::string str_prog_name="/usr/bin/top")
+{
+ std::vector<char *> cmd; // stackoverflow.com/questions/5846934/how-to-pass-a-vector-to-execvp
+ std::stringstream s(str_prog_name);
+ std::string word="";
+
+ for (int i = 0; s >> word; i++)
+ {
+  char* chrword=new char[ word.size() ];
+  std::strncpy(chrword,word.c_str(),word.size());
+  cmd.push_back(chrword);
+ }
+
+// cmd.push_back("echo");
+//cmd.push_back("echo");
+// cmd.push_back("echo");
+ cmd.push_back(NULL);
+
+ char **argsx = &cmd[0];
+ int pid = fork();
+ if (pid==-1){std::cerr<<"Fork Failed somehow!!";}
+ if (pid==0) //only true for the forked process->launch
+  {std::cerr<<"Launching process" ;
+   int rc = execvp(argsx[0],argsx);
+   std::cerr<<"Output of fork is "<<rc<<std::endl;
+   if (rc==-1) std::cout<<"Error launching process "<<argsx[0]<<" Did you have the path right?"<<std::endl;
+  }
+
+ return pid;
+}
+
+
 void
 getPiD(std::string& str_pid, std::string& str_pname)
 {
@@ -160,7 +194,7 @@ int launch_from_config(std::string configfile,std::string& delays,int& delay, st
 
 int
 main(int argc, char *argv[]) {
- std::string str_pid,configfile="",str_pname="",delays,str_command,metric;
+ std::string str_pid,configfile="",str_launch="",str_pname="",delays,str_command,metric;
  int delay=1000000;
  while(1)
  {
@@ -205,9 +239,9 @@ main(int argc, char *argv[]) {
  	  str_pname=optarg;
            break;
  
-         case 'l':
+         case 'l': //doesnt work fully
            std::cerr<<"Launching process   "<<optarg<<std::endl;
-           str_pname=optarg;
+           str_launch=optarg; 
            break;
  
          case 'd':
@@ -218,13 +252,13 @@ main(int argc, char *argv[]) {
  
          case 'm':
            std::cerr<<"OpenTSDB metric set as "<< optarg<<std::endl;
- 	  metric=optarg;
+           metric=optarg;
            break;
  
          case 'c':
            std::cerr<<"Config File is "<<optarg<<std::endl;
            configfile=optarg;
- 	  launch_from_config(configfile,delays,delay,str_pname, str_pid);
+ 	   launch_from_config(configfile,delays,delay,str_pname, str_pid);
            break;
  
          case '?':
@@ -239,6 +273,13 @@ main(int argc, char *argv[]) {
 
 
   std::cerr<<"$proc "<<str_pid<<" "<<str_pname<< " from "<<configfile<<" with delay (ms) "<<delay/1000<<std::endl;
+  getPiD(str_pid,str_pname);
+  if (str_launch!=""){
+   std::cerr<< "launching process "<< str_launch<<std::endl;
+   str_pid=std::to_string(launch_process(str_launch));
+   str_pname=str_launch;
+  }
+
 //  return 0;
   std::ifstream filec("/proc/"+str_pid+"/cmdline");
   filec>>str_command;
@@ -248,17 +289,16 @@ main(int argc, char *argv[]) {
   std::cerr<<"$proc-i rchar, wchar, syscr, syscw, read_bytes, write_bytes\n";
   std::cerr<<"$proc-s state, minflt, mjflt, utime ,s_time, nthreads, VMSize, RSS(pages)'\n';";
 // rusage ru;
- time_t t = time(0);
- struct tm * now = localtime( & t );
- std::cerr <<"$proc-Start time: "<< (now->tm_hour)<<":"<<(now->tm_min)<<":"<<(now->tm_sec) << std::endl; 
- std::remove_if(configfile.begin(), configfile.end(), isspace);
- if (metric.empty()){
-  metric="exe."+str_pname+"."+str_pid;}
- else {
-  metric="exe."+metric+"."+str_pid;}
+  time_t t = time(0);
+  struct tm * now = localtime( & t );
+  std::cerr <<"$proc-Start time: "<< (now->tm_hour)<<":"<<(now->tm_min)<<":"<<(now->tm_sec) << std::endl; 
+  std::remove_if(configfile.begin(), configfile.end(), isspace);
+  if (metric.empty()){
+   metric="exe."+str_pname+"."+str_pid;}
+  else {
+   metric="exe."+metric+"."+str_pid;}
 
-
- std::fstream tsdbfile;
+  std::fstream tsdbfile;
 
  while(not(kill(std::stoi(str_pid),NULL))) //continues if pid exists, not sure if works fasters
  {
